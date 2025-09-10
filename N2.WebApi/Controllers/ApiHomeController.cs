de426cb3ae020bbd302b980a8c484e9300df03cf
dev <dev@dev.com>
2025-09-10 22:23:14 +0800

    update

---
+++ N2.WebApi/Controllers/ApiHomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using N2.Core.DbContext;
+using Microsoft.AspNetCore.Authorization;
+
+namespace N2.WebApi.Controllers
+{
+    [AllowAnonymous]
+    public class ApiHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return new RedirectResult("/swagger/");
+        }
+
+    }
+}
